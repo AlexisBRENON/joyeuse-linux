b3a0ddc16643296ff88263e18de61a8733df0431
Alexis BRENON <alexis.brenon@adyoulike.com>
2021-09-19 11:43:12 +0200

    Add french localized file

---
+++ l11n/fr.sh
@@ -0,0 +1,26 @@
+
+export JOY_UPD_FW_START="Téléchargement du fichier microcode"
+export JOY_UPD_FW_FAIL="Impossible de trouver le fichier microcode"
+export JOY_UPD_FW_DONE='Fichier microcode trouvé: \"${1}\"'
+
+export JOY_UPD_SEARCH_START="Recherche du périphérique Joyeuse..."
+export JOY_UPD_SEARCH_FAIL="Aucun périphérique Joyeuse trouvé"
+export JOY_UPD_SEARCH_DONE='Périphérique trouvé: \"${1}\"'
+
+export JOY_UPD_BAK_START='Début de la sauvegarde de \"${1}\" vers \"${2}\"'
+export JOY_UPD_BAK_DONE="Sauvegarde réalisée avec succès"
+
+export JOY_UPD_FORMAT_DONE="Formatage du périphérique réalisé avec succès"
+
+export JOY_UPD_BOOT_UPGRADE="Fichier upgrade.txt créé avec succès"
+export JOY_UPD_BOOT_REPLUG="Débranchez \(tu-di-tu-duu\) et rebranchez \(tu...tu...tu...tuu\) le périphérique"
+
+export JOY_UPD_BOOT_DETECT="Recherche du périphérique en mode programmable"
+export JOY_UPD_BOOT_FAIL="Impossible de trouver un périphérique programmable..."
+export JOY_UPD_BOOT_FOUND="Périphérique programmable STM32 USB trouvé"
+
+export JOY_UPD_UPDATE_DONE="Mise à jour du microcode réalisée avec succès"
+export JOY_UPD_UPDATE_REPLUG="Débranchez et rebranchez le périphérique \(tu-tu-tu-ti-tu-tuu\)..."
+
+export JOY_UPD_RESTORE_START='Début de la restauration des données de \"${1}\" vers \"${2}\"'
+export JOY_UPD_RESTORE_DONE="Restauration réalisée avec succès"
